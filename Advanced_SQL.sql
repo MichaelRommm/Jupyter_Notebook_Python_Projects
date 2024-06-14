5475bb8d31fbe5e50ea1bae0b88c4e6f97c59613
Michael Romm <165962651+MichaelRommm@users.noreply.github.com>
2024-06-14 20:20:56 +0300

    Update Advanced_SQL.sql

--- Advanced_SQL.sql
+++ Advanced_SQL.sql
@@ -20,6 +20,8 @@ WHERE p.ProductID NOT IN
 		 JOIN Production.Product as pp ON pp.ProductID=sof.ProductID)
 ORDER BY p.ProductID;
 
+[q1](images/q1.jpg)\
+
 --- Construct a SQL query to fetch the CustomerID, Last Name, and First Name of customers who have not placed any sales orders:
 
 SELECT 
